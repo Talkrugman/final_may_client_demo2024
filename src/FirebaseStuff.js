6ea8d8d4a0da445e878d6d12419efb51be6cbe51
tal krugman <talkrugman@gmail.com>
2024-01-17 13:43:50 +0200

    second commite

---
+++ src/FirebaseStuff.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhw8Fznfk9FNOWfP2oSZGQHL_9tszre64",
+  authDomain: "final-may-demo2024.firebaseapp.com",
+  projectId: "final-may-demo2024",
+  storageBucket: "final-may-demo2024.appspot.com",
+  messagingSenderId: "1071266140343",
+  appId: "1:1071266140343:web:deb4d09d75133e9d207d32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
